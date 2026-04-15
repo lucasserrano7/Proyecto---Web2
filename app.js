0e54f5fa3f0a8a09b7faeb1769fb5dc72db829b9
Lucas <serranolucas1000@gmail.com>
2026-04-15 20:12:43 -0300

    ignorando env y modules

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const pug = require("pug");
-
+// CONSTANTES
 const app = express();
+const PORT = 3000;
 
+// 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//MOTOR DE PLANTILLAS
 app.set('view engine',  'pug');
 app.set('views',  './views');
 app.use(express.static('public'))
@@ -15,7 +18,7 @@ app.get('/',( req, res)=> {
     res.render('layout')
 });
 
-const PORT = 3000;
+
 
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en el puerto:${PORT}`)
