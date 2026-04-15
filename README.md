88cdc45038b0072a743d4bb63346f8914553cbff
Lucas <serranolucas1000@gmail.com>
2026-04-15 19:53:14 -0300

    dandole otrden a las carpetas, css, y mas en readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Proyecto---Web2
+
+Es una aplicacion del tipo red social que esta en proceso je
+:)
--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require("express");
 const pug = require("pug");
 
 const app = express();
-const compiledFunction = pug.compileFile("template.pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine',  'pug');
+app.set('views',  './views');
+app.use(express.static('public'))
+
+
+
+app.get('/',( req, res)=> {
+    res.render('layout')
+});
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto:${PORT}`)
+})
