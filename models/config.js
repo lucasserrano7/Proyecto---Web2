743c17d0e6925ecf2fd5958b5f61ec03805f9715
Lucas <serranolucas1000@gmail.com>
2026-06-06 15:43:10 -0300

    cambio variables conexion a base de datos

--- models/config.js
+++ models/config.js
@@ -4,21 +4,22 @@ import { Sequelize } from "sequelize";
 import * as pg from "pg";
 
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(process.env.DB_URL,{
     dialect: 'postgres',
     dialectModule: pg,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_PORT, 10),
+    dialectOptions:{
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 })
 
-// try {
-//   await sequelize.authenticate();
-//   console.log('Connection has been established successfully.');
-// } catch (error) {
-//   console.error('Unable to connect to the database:', error);
-// }
+ try {
+   await sequelize.authenticate();
+   console.log('Connection has been established successfully.');
+ } catch (error) {
+   console.error('Unable to connect to the database:', error);
+ }
 
 export default sequelize;
