f609dbaec9218197c1357c009678fc2bd5ba08fe
Lucas <serranolucas1000@gmail.com>
2026-06-06 16:02:54 -0300

    mas correciones

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 //MOTOR DE PLANTILLAS
 app.set("view engine", "pug");
-app.set("views", path.join(process.cwd()), "./views");
+app.set("views", path.join(process.cwd()), ".views");
 
 
 app.use((req, res, next)=>{
