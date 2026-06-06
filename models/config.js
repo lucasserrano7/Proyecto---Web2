814b91d9de20cf9c3a0a3acd673f07afd5acf11b
Lucas <serranolucas1000@gmail.com>
2026-06-06 15:51:48 -0300

    congif actualizado

--- models/config.js
+++ models/config.js
@@ -3,14 +3,6 @@ import'dotenv/config';
 import { Sequelize } from "sequelize";
 import * as pg from "pg";
 
-const sslConn = process.env.DB_SSL === 'true' ? {
-    ssl: {
-    require: true,
-    rejectUnauthorized: false,
-    }
-} : undefined;
-console.log('DB_SSL:', process.env.DB_SSL);
-console.log('sslConn:', sslConn);
 
 const sequelize = new Sequelize(process.env.DB_URL,{
     dialect: 'postgres',
